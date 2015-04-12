7046afdf835767bed86f7541ea77363654b187cc
khangnhfuky <khangnhfuky@users.noreply.github.com>
2015-04-12 17:11:51 +0700

    Create project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    private Animator animator;
+    public RuntimeAnimatorController[] listAnimator;
+    public int state;
+	void Start () {
+        animator = gameObject.GetComponent<Animator>();
+        animator.runtimeAnimatorController = listAnimator[state];
+	}
+	
+	void Update () {
+	
+	}
+}
