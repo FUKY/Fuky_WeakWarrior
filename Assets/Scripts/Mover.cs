e1b2a6f5756f2329495599ed3479f6332295b47a
khangnhfuky <khangnhfuky@users.noreply.github.com>
2015-04-16 02:38:11 +0700

    Enemy1 Move + Attack

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,15 +2,14 @@
 using System.Collections;
 
 public class Mover : MonoBehaviour {
-    public float speed;
-   
-	// Use this for initialization
-	void Start () {
-	    
-	}
-	
-	// Update is called once per frame
+    public float speed = 3f;
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+    }
 	void Update () {
-	
+        rb2d.velocity = new UnityEngine.Vector2(speed, rb2d.velocity.y);
 	}
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,9 +5,12 @@ public class PlayerController : MonoBehaviour {
     private Animator animator;
     public RuntimeAnimatorController[] listAnimator;
     public int state;
+    private bool attack;
+
 	void Start () {
         animator = gameObject.GetComponent<Animator>();
         animator.runtimeAnimatorController = listAnimator[state];
+        animator.SetBool("attack", attack = false);
 	}
 	
 	void Update () {
